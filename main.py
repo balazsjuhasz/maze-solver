a01c262613f153fe9fdcce8b1a26db53b26a072d
Balazs Juhasz <juhaszbal@gmail.com>
2023-10-05 10:20:40 +0200

    Set up basic project layout

---
+++ main.py
@@ -0,0 +1,27 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__canvas = Canvas(self.__root, width=width, height=height)
+        self.__canvas.pack()
+        self.is_running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+    
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.is_running = True
+        while self.is_running:
+            self.redraw()
+
+    def close(self):
+        self.is_running = False
+
+
+if __name__ == "__main__":
+    win = Window(800, 600)
+    win.wait_for_close()
