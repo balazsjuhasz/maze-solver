1502bf9b1691cb45dbe14bef1af28e1e6a171f01
Balazs Juhasz <juhaszbal@gmail.com>
2023-10-05 11:43:49 +0200

    Create cells for maze

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from models import Window, Line, Point
+from models import Window, Point, Cell
 
 if __name__ == "__main__":
     win = Window(800, 600)
-    line1 = Line(Point(400, 300), Point(0, 0))
-    line2 = Line(Point(400, 300), Point(800, 0))
-    line3 = Line(Point(400, 300), Point(800, 600))
-    line4 = Line(Point(400, 300), Point(0, 600))
-    win.draw_line(line1, "black")
-    win.draw_line(line2, "red")
-    win.draw_line(line3, "green")
-    win.draw_line(line4, "blue")
+    cell2 = Cell(Point(200, 100), Point(300, 200), win)
+    cell2.has_right_wall = False
+    cell2.draw()
+
+    cell4 = Cell(Point(300, 100), Point(400, 200), win)
+    cell4.has_left_wall = False
+    cell4.draw()
+
+    cell2.draw_move(cell4)
+
     win.wait_for_close()
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from tkinter import Tk, BOTH, Canvas
+from __future__ import annotations
+from tkinter import Tk, Canvas
 
 
 class Point:
@@ -25,16 +26,16 @@ class Line:
 
 class Window:
     def __init__(self, width: int, height: int):
-        self.__root = Tk()
-        self.__root.title("Maze Solver")
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        self.__canvas = Canvas(self.__root, width=width, height=height)
-        self.__canvas.pack()
-        self.__is_running = False
+        self._root = Tk()
+        self._root.title("Maze Solver")
+        self._root.protocol("WM_DELETE_WINDOW", self.close)
+        self._canvas = Canvas(self._root, width=width, height=height)
+        self._canvas.pack()
+        self._is_running = False
 
     def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
+        self._root.update_idletasks()
+        self._root.update()
 
     def wait_for_close(self):
         self.__is_running = True
@@ -45,4 +46,69 @@ class Window:
         self.__is_running = False
 
     def draw_line(self, line: Line, fill_color: str):
-        line.draw(self.__canvas, fill_color)
+        line.draw(self._canvas, fill_color)
+
+
+class Cell:
+    def __init__(self, top_left: Point, bottom_right: Point, win: Window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = top_left.x
+        self._y1 = top_left.y
+        self._x2 = bottom_right.x
+        self._y2 = bottom_right.y
+        self._win = win
+
+    def draw(self):
+        if self.has_left_wall:
+            self._win.draw_line(
+                Line(
+                    Point(self._x1, self._y1),
+                    Point(self._x1, self._y2),
+                ),
+                "black",
+            )
+        if self.has_right_wall:
+            self._win.draw_line(
+                Line(
+                    Point(self._x2, self._y1),
+                    Point(self._x2, self._y2),
+                ),
+                "black",
+            )
+        if self.has_top_wall:
+            self._win.draw_line(
+                Line(
+                    Point(self._x1, self._y1),
+                    Point(self._x2, self._y1),
+                ),
+                "black",
+            )
+        if self.has_bottom_wall:
+            self._win.draw_line(
+                Line(
+                    Point(self._x1, self._y2),
+                    Point(self._x2, self._y2),
+                ),
+                "black",
+            )
+
+    def draw_move(self, to_cell: Cell, undo: bool = False):
+        center_x = (self._x1 + self._x2) // 2
+        center_y = (self._y1 + self._y2) // 2
+        other_center_x = (to_cell._x1 + to_cell._x2) // 2
+        other_center_y = (to_cell._y1 + to_cell._y2) // 2
+        if undo:
+            color = "gray"
+        else:
+            color = "red"
+
+        self._win.draw_line(
+            Line(
+                Point(center_x, center_y),
+                Point(other_center_x, other_center_y),
+            ),
+            color,
+        )
